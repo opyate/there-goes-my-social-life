b4fc552325e048421b8d199938f35c62101f13ac
Juan Uys <opyate@gmail.com>
2011-02-06 12:38:28 +0000

    deploy script scp and cap combined

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,6 @@ VERSION=$(echo "$(ls -lat target/scala_2.8.1/ | grep war | head -n1)" | sed 's/.
 
 echo "Version = ${VERSION}"
 
-scp target/scala_2.8.1/bopango-website_2.8.1-${VERSION}.war root@bopango.net:~/bopango-deployments/bopango-v${VERSION}.war
-
-cap deploy -s version=${VERSION}
+scp target/scala_2.8.1/bopango-website_2.8.1-${VERSION}.war root@bopango.net:~/bopango-deployments/bopango-v${VERSION}.war && cap deploy -s version=${VERSION}
 
 echo "Done."
